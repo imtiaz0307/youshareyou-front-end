983942018dd84ca337de07b62353534c67f70f3c
imtiaz0307 <imtiaz0307@gmail.com>
2022-10-06 15:34:56 +0500

    Favicon added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <Hero 
     slogan='Sharing Is Caring, Either Way!' 
-    description="Hello Let's share your feelings, stories, incidents, funny moments, or whatever you feel to share. You can also read fellow member's stories. " 
+    description="Let's share your feelings, stories, incidents, funny moments, or whatever you feel to share. You can also read fellow member's stories. " 
     buttonText="Let's Read Stories" />
   )
 }
