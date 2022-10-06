7b65290f51c8ad97bc17ee52e043a58a2a8fba0e
imtiaz0307 <imtiaz0307@gmail.com>
2022-10-06 15:27:37 +0500

    Fixed Some issues

--- src/Components/Story/Story.jsx
+++ src/Components/Story/Story.jsx
@@ -13,26 +13,26 @@ const Story = ({ details }) => {
       <div className='story' ref={story}>
         <div className="story-details">
           <div className="story-top">
-            <p className='story-user'>Posted by: {details[0].username}</p>
+            <p className='story-user'>Posted by: {details[0]?.username}</p>
             {details[1].user === currentUserId && (
               <div className="story-delete-edit">
                 <p className='edit-story' onClick={() => {
                   setShowEditModal(true)
-                  console.log(details[1]._id)
-                  setEditedTitle(details[1].title)
-                  setEditedDescription(details[1].description)
-                  setEditPostId(details[1]._id)
+                  console.log(details[1]?._id)
+                  setEditedTitle(details[1]?.title)
+                  setEditedDescription(details[1]?.description)
+                  setEditPostId(details[1]?._id)
                 }}><i className="fa-solid fa-pen-to-square"></i></p>
                 <p className='delete-story' onClick={() => {
                   let confirmation = window.confirm('Are you sure you want to delete this post?')
-                  confirmation && deletePost(details[1]._id) && story.current.remove() && getAllPosts()
+                  confirmation && deletePost(details[1]?._id) && story.current.remove() && getAllPosts()
                 }}><i className="fa-sharp fa-solid fa-trash"></i></p>
               </div>
             )}
           </div>
-          <p className='story-time'>{details[1].postedAt.split('T')[0]}</p>
-          <p className="story-title">{details[1].title}</p>
-          <p className='story-description'>{details[1].description}</p>
+          <p className='story-time'>{details[1]?.postedAt.split('T')[0]}</p>
+          <p className="story-title">{details[1]?.title}</p>
+          <p className='story-description'>{details[1]?.description}</p>
         </div>
       </div>
       <EditModal/>
